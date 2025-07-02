43e8335a7743e534effc4637953712d79ea9499b
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2025-07-02 15:55:33 +0200

    Fixed loot tables

--- src/main/java/me/emafire003/dev/ohmymeteors/OhMyMeteors.java
+++ src/main/java/me/emafire003/dev/ohmymeteors/OhMyMeteors.java
@@ -21,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import java.nio.file.Path;
 
 public class OhMyMeteors implements ModInitializer {
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
