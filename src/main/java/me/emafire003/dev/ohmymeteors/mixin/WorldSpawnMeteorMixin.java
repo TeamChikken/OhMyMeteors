36cd79e6f8586bcaa81233266026708ce1a9e7cf
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2025-08-23 15:10:43 +0200

    Added config options to have black/whitelisted biomes in which meteors can spawn, as well as setting custom spawn chances for each biome

--- src/main/java/me/emafire003/dev/ohmymeteors/mixin/WorldSpawnMeteorMixin.java
+++ src/main/java/me/emafire003/dev/ohmymeteors/mixin/WorldSpawnMeteorMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.ChunkManager;
 import net.minecraft.world.dimension.DimensionType;
 import org.jetbrains.annotations.Nullable;
@@ -59,39 +60,37 @@ public abstract class WorldSpawnMeteorMixin extends World implements StructureWo
             return; //Hey. this return is important. I totally haven't discovered i forgot to put it here because like 200 meteors spawned in the span of a second in my face. Not at all.
         }
 
-        AtomicBoolean dimension_ok = new AtomicBoolean(false);
         //TODO check if it's a problem performance wise?
         PlayerEntity p = this.getRandomAlivePlayer();
         if(p == null){
             return;
         }
 
+        /// As for spawning, dimension overrides biome
         RegistryEntry<DimensionType> current_dim = p.getWorld().getDimensionEntry();
 
-        //Checks all the dimensions specified in the config file. As soon as it finds one, sets dimension ok to true
-        //and then stops checking
-        //todo this will check every time. If i add per-dimension chances it's ok.
-        Config.SPAWN_DIMENSIONS.forEach(dim -> {
-            if(dimension_ok.get()){{
-                return;
-            }}
-            if(dim.equals(current_dim.getIdAsString())){
-                dimension_ok.set(true);
-            }
-        }
-        );
-
-        //If dimension_ok is still false, it means  the meteor can't spawn here so set it to true
-        if(!dimension_ok.get()){
+        if(!checkDimension(current_dim)){
             return;
         }
 
+        RegistryEntry<Biome> current_biome = p.getWorld().getBiome(p.getBlockPos());
 
+        if(!checkBiome(current_biome)){
+            return;
+        }
+
+        /// For the chance, biome > dim > global
+        /// Aka the biome chance overrides the dimension chance which in turn overrides the global parameter
         int chance = Config.METEOR_SPAWN_CHANCE;
 
-        //If the meteor is in the map, it will override the chance thing
+        //If the current dimension is in the map, it will override the chance thing
         if(Config.DIMENSION_CHANCES.containsKey(current_dim.getIdAsString())){
             chance = Config.DIMENSION_CHANCES.get(current_dim.getIdAsString());
+        }
+
+        //If the biome is in the map the chance gets overridden again
+        if(Config.BIOME_CHANCES.containsKey(current_biome.getIdAsString())){
+            chance = Config.BIOME_CHANCES.get(current_biome.getIdAsString());
         }
 
         if(Config.MODIFY_SPAWN_CHANCE_AT_NIGHT && this.isNight()){
@@ -99,14 +98,52 @@ public abstract class WorldSpawnMeteorMixin extends World implements StructureWo
             if(Config.DIMENSION_NIGHT_CHANCES.containsKey(current_dim.getIdAsString())){
                 chance = Config.DIMENSION_NIGHT_CHANCES.get(current_dim.getIdAsString());
             }
+            if(Config.BIOME_NIGHT_CHANCES.containsKey(current_biome.getIdAsString())){
+                chance = Config.BIOME_NIGHT_CHANCES.get(current_biome.getIdAsString());
+            }
         }
-
 
         if(this.getRandom().nextBetween(0, chance) == 0){
             spawnMeteor(((ServerWorld) (Object) this), p);
             if(Config.SHOULD_COOLDOWN_BETWEEN_METEORS){
                 meteorCooldown = 20*Config.MIN_METEOR_COOLDOWN_TIME;
             }
+        }
+    }
+
+    protected WorldSpawnMeteorMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates) {
+        super(properties, registryRef, registryManager, dimensionEntry, profiler, isClient, debugWorld, biomeAccess, maxChainedNeighborUpdates);
+    }
+
+    @Unique
+    public boolean checkDimension(RegistryEntry<DimensionType> current_dim){
+        //Checks all the dimensions specified in the config file. As soon as it finds one, sets dimension ok to true
+        //and then stops checking
+        AtomicBoolean dimension_ok = new AtomicBoolean(false);
+        Config.SPAWN_DIMENSIONS.forEach(dim -> {
+                    if(dimension_ok.get()){{
+                        return;
+                    }}
+                    if(dim.equals(current_dim.getIdAsString())){
+                        dimension_ok.set(true);
+                    }
+                }
+        );
+
+        return dimension_ok.get();
+    }
+
+    @Unique
+    public boolean checkBiome(RegistryEntry<Biome> current_biome){
+        //Checks all the dimensions specified in the config file. As soon as it finds one, sets dimension ok to true
+        //and then stops checking
+
+        //If true means whitelist aka it HAS to be present
+        //if false means in MUST NOT be present
+        if(Config.BIOME_LIST_MODE){
+            return Config.BIOME_SPAWN_LIST.contains(current_biome.getIdAsString());
+        }else{
+            return !Config.BIOME_SPAWN_LIST.contains(current_biome.getIdAsString());
         }
     }
 }
