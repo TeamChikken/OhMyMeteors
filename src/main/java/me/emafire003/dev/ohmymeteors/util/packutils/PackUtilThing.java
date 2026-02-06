c2f5c53b0af332c7e20e867e646c8c03e32635ce
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2026-02-06 13:21:21 +0100

    Added proper datapack generation to teh CustomStructureCommand

---
+++ src/main/java/me/emafire003/dev/ohmymeteors/util/packutils/PackUtilThing.java
@@ -0,0 +1,10 @@
+package me.emafire003.dev.ohmymeteors.util.packutils;
+
+public class PackUtilThing {
+    public PackUtilThing(PackMeta pack) {
+        this.pack = pack;
+    }
+
+    PackMeta pack;
+
+}
