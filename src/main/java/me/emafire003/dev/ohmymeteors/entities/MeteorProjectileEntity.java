60ce9a67d2ce7a923f795138b273f497cbcb007d
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2026-02-07 20:05:28 +0100

    Added two new better ways to spawn meteor showers

--- src/main/java/me/emafire003/dev/ohmymeteors/entities/MeteorProjectileEntity.java
+++ src/main/java/me/emafire003/dev/ohmymeteors/entities/MeteorProjectileEntity.java
@@ -9,6 +9,7 @@ import me.emafire003.dev.ohmymeteors.events.MeteorSpawnEvent;
 import me.emafire003.dev.ohmymeteors.config.Config;
 import me.emafire003.dev.ohmymeteors.util.ExplosionUtils;
 import me.emafire003.dev.structureplacerapi.StructurePlacerAPI;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -32,10 +33,7 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
-import net.minecraft.util.BlockMirror;
-import net.minecraft.util.BlockRotation;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.Identifier;
+import net.minecraft.util.*;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.*;
@@ -50,6 +48,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static me.emafire003.dev.ohmymeteors.OhMyMeteors.METEOR_STRUCTURES;
 
@@ -672,10 +671,69 @@ public class MeteorProjectileEntity extends ExplosiveProjectileEntity {
         return this.getSize() > Config.MAX_BIG_METEOR_SIZE;
     }
 
+
+    /**Used to get a random meteor position and velocity oriented downwards
+     *
+     * @return a Pair, where the first value is the Position and teh second one the Velocity*/
+    private static Pair<Vec3d, Vec3d> getDownwardsMeteorPosAndVelocity(Vec3d originPos, ServerWorld world, int min_spawn_d, int max_spawn_d, double spawn_height){
+        //The invert is to also have a chance at having negative coordinates, otherwise they would always be positive
+        int invert_x = 1;
+        if(world.getRandom().nextBoolean()){
+            invert_x = -1;
+        }
+
+        int invert_z = 1;
+        if(world.getRandom().nextBoolean()){
+            invert_z = -1;
+        }
+
+        Vec3d pos = new Vec3d(originPos.getX()+world.getRandom().nextBetween(min_spawn_d, max_spawn_d)*invert_x,
+                spawn_height,
+                originPos.getZ()+world.getRandom().nextBetween(min_spawn_d, max_spawn_d)*invert_z
+        );
+
+        invert_x = 1;
+        if(world.getRandom().nextBoolean()){
+            invert_x = -1;
+        }
+
+        invert_z = 1;
+        if(world.getRandom().nextBoolean()){
+            invert_z = -1;
+        }
+        Vec3d vel = new Vec3d((world.getRandom().nextFloat()/2)*invert_x, -1.0f*(world.getRandom().nextFloat()+Config.DOWNWARDS_SPEED_MODIFIER), (world.getRandom().nextFloat()/2)*invert_z);
+
+        return new Pair<>(pos, vel);
+
+    }
+
     /**
      * Gets a meteor object to be spawned in, with a velocity oriented downwards and a spawn position already set up
      * */
     public static MeteorProjectileEntity getDownwardsMeteor(Vec3d originPos, ServerWorld world, int min_spawn_d, int max_spawn_d, double spawn_height, int min_size, int max_size, boolean homing){
+        MeteorProjectileEntity meteor = new MeteorProjectileEntity(world);
+
+        Pair<Vec3d, Vec3d> pos_vel = getDownwardsMeteorPosAndVelocity(originPos, world, min_spawn_d, max_spawn_d, spawn_height);
+
+        meteor.setPos(pos_vel.getLeft().x, pos_vel.getLeft().y, pos_vel.getLeft().z);
+
+        meteor.setSize(world.getRandom().nextBetween(Math.max(0, min_size), Math.min(50, max_size)));
+
+        meteor.setVelocity(pos_vel.getRight());
+
+        if(homing){
+            //TODO maybe just go with 1,1,1 as velocity multiplier
+            meteor.setVelocity(originPos.subtract(meteor.getPos()).normalize().multiply(meteor.getVelocity().getX(), meteor.getVelocity().getY()*-1, meteor.getVelocity().getZ()));
+        }
+
+        return meteor;
+    }
+
+    /**Used when spawning an orderd meteor shower where most meteors share the same general direction as the previous one
+     * <p>
+     * Returns a new meteor object with a general direction similar to the specified one, but slightly different origin and velocity
+     * The spawn distances are for the new spread, so keep the generally low*/
+    public static MeteorProjectileEntity getDownwardsMeteorSameDirection(Vec3d prev_origin, Vec3d prev_vel, ServerWorld world, int min_spawn_d, int max_spawn_d, double spawn_height, int min_size, int max_size, boolean homing){
         MeteorProjectileEntity meteor = new MeteorProjectileEntity(world);
 
         //The invert is to also have a chance at having negative coordinates, otherwise they would always be positive
@@ -684,14 +742,14 @@ public class MeteorProjectileEntity extends ExplosiveProjectileEntity {
             invert_x = -1;
         }
 
-        int invert_y = 1;
+        int invert_z = 1;
         if(world.getRandom().nextBoolean()){
-            invert_y = -1;
+            invert_z = -1;
         }
 
-        meteor.setPos(originPos.getX()+world.getRandom().nextBetween(min_spawn_d, max_spawn_d)*invert_x,
+        Vec3d pos = new Vec3d(prev_origin.getX()+world.getRandom().nextBetween(min_spawn_d, max_spawn_d)*invert_x,
                 spawn_height,
-                originPos.getZ()+world.getRandom().nextBetween(min_spawn_d, max_spawn_d)*invert_y
+                prev_origin.getZ()+world.getRandom().nextBetween(min_spawn_d, max_spawn_d)*invert_z
         );
 
         invert_x = 1;
@@ -699,18 +757,21 @@ public class MeteorProjectileEntity extends ExplosiveProjectileEntity {
             invert_x = -1;
         }
 
-        invert_y = 1;
+        invert_z = 1;
         if(world.getRandom().nextBoolean()){
-            invert_y = -1;
+            invert_z = -1;
         }
+        Vec3d vel_modifier = new Vec3d((world.getRandom().nextFloat()/5)*invert_x, -1.0f*(world.getRandom().nextFloat()/5), (world.getRandom().nextFloat()/5)*invert_z);
+
+
+        meteor.setPos(pos.x, pos.y, pos.z);
 
         meteor.setSize(world.getRandom().nextBetween(Math.max(0, min_size), Math.min(50, max_size)));
 
-        meteor.setVelocity((world.getRandom().nextFloat()/2)*invert_x, -1.0f*(world.getRandom().nextFloat()+Config.DOWNWARDS_SPEED_MODIFIER), (world.getRandom().nextFloat()/2)*invert_y);
+        meteor.setVelocity(vel_modifier.add(prev_vel));
 
         if(homing){
-            //TODO maybe just go with 1,1,1 as velocity multiplier
-            meteor.setVelocity(originPos.subtract(meteor.getPos()).normalize().multiply(meteor.getVelocity().getX(), meteor.getVelocity().getY()*-1, meteor.getVelocity().getZ()));
+            meteor.setVelocity(prev_origin.subtract(meteor.getPos()).normalize().multiply(meteor.getVelocity().getX(), meteor.getVelocity().getY()*-1, meteor.getVelocity().getZ()));
         }
 
         return meteor;
@@ -759,11 +820,104 @@ public class MeteorProjectileEntity extends ExplosiveProjectileEntity {
         world.spawnEntity(meteor);
     }
 
-    public static void spawnMeteorShower(ServerWorld world, PlayerEntity p){
+    /**Spawns a meteor shower where all meteors spawn at the same time in random directions around the point of origin
+     * Also check out {@link #spawnMeteorShowerDelayed(ServerWorld, PlayerEntity)} and {@link #spawnMeteorShowerDelayedDirection(ServerWorld, PlayerEntity)}*/
+    public static void spawnMeteorShowerInstant(ServerWorld world, PlayerEntity p){
         int r = world.getRandom().nextBetween(Config.MIN_METEORS_IN_SHOWER, Config.MAX_METEORS_IN_SHOWER);
         for(int i = 0; i < r; i++){
             spawnMeteor(world, p, true);
         }
+        String message = "message.ohmymeteors.meteor_shower_spawned";
+        if(Config.ANNOUNCE_METEOR_SPAWN){
+            if(Config.ANNOUNCE_LOCATION){
+                String pos = p.getBlockPos().getX() + " x, " + p.getBlockPos().getZ() + " z!";
+                world.getPlayers().forEach(player -> player.sendMessage(Text.literal(OhMyMeteors.PREFIX).append(Text.translatable(message+".localized", pos).formatted(Formatting.RED)), Config.ACTIONBAR_ANNOUNCEMENTS));
+            }else{
+                world.getPlayers().forEach(player -> player.sendMessage(Text.literal(OhMyMeteors.PREFIX).append(Text.translatable(message).formatted(Formatting.RED)), Config.ACTIONBAR_ANNOUNCEMENTS));
+            }
+        }
+    }
+
+    /** Spawns a meteor shower with meteors going in random directions but with a slight delay between them
+     * unlike {@link #spawnMeteorShowerInstant(ServerWorld, PlayerEntity)} where all meteors spawn at the same time.
+     * Using {@link #spawnMeteorShowerDelayedDirection(ServerWorld, PlayerEntity)} will also have them follow the same general direction*/
+    public static void spawnMeteorShowerDelayed(ServerWorld world, PlayerEntity p){
+        int total = world.getRandom().nextBetween(Config.MIN_METEORS_IN_SHOWER, Config.MAX_METEORS_IN_SHOWER);
+        AtomicInteger ticker = new AtomicInteger();
+        AtomicInteger spawned_meteors = new AtomicInteger();
+        int base_spawn_delay = 15;
+        AtomicInteger random_spawn_delay = new AtomicInteger(world.getRandom().nextBetween(-10, +10));
+
+        spawnMeteor(world, p, true);
+        spawned_meteors.getAndIncrement();
+
+        ServerTickEvents.END_SERVER_TICK.register((server) -> {
+            if(ticker.get() == -1 || spawned_meteors.get() >= total){
+                return;
+            }
+            if(ticker.get() == base_spawn_delay+ random_spawn_delay.get()){
+                if(spawned_meteors.get() >= total){
+                    ticker.set(-1);
+                    return;
+                }
+                spawnMeteor(world, p, true);
+                spawned_meteors.getAndIncrement();
+                ticker.set(0);
+                random_spawn_delay.set(world.getRandom().nextBetween(-10, +10));
+            }
+            ticker.getAndIncrement();
+        });
+
+        String message = "message.ohmymeteors.meteor_shower_spawned";
+        if(Config.ANNOUNCE_METEOR_SPAWN){
+            if(Config.ANNOUNCE_LOCATION){
+                String pos = p.getBlockPos().getX() + " x, " + p.getBlockPos().getZ() + " z!";
+                world.getPlayers().forEach(player -> player.sendMessage(Text.literal(OhMyMeteors.PREFIX).append(Text.translatable(message+".localized", pos).formatted(Formatting.RED)), Config.ACTIONBAR_ANNOUNCEMENTS));
+            }else{
+                world.getPlayers().forEach(player -> player.sendMessage(Text.literal(OhMyMeteors.PREFIX).append(Text.translatable(message).formatted(Formatting.RED)), Config.ACTIONBAR_ANNOUNCEMENTS));
+            }
+        }
+    }
+
+    /**Spawns meteor showers that generally go in the same direction each delayed by a bit*/
+    public static void spawnMeteorShowerDelayedDirection(ServerWorld world, PlayerEntity p){
+        int total = world.getRandom().nextBetween(Config.MIN_METEORS_IN_SHOWER, Config.MAX_METEORS_IN_SHOWER);
+        AtomicInteger ticker = new AtomicInteger();
+        AtomicInteger spawned_meteors = new AtomicInteger();
+        int base_spawn_delay = 15;
+        AtomicInteger random_spawn_delay = new AtomicInteger(world.getRandom().nextBetween(-10, +10));
+
+        Pair<Vec3d, Vec3d> prev = getDownwardsMeteorPosAndVelocity(p.getPos(), world.toServerWorld(),
+                Config.MIN_METEOR_SPAWN_DISTANCE, Config.MAX_METEOR_SPAWN_DISTANCE, Config.METEOR_SPAWN_HEIGHT);
+
+        AtomicInteger limit_a = new AtomicInteger(world.getRandom().nextBetween(Config.MIN_METEOR_SPAWN_DISTANCE, Config.MAX_METEOR_SPAWN_DISTANCE));
+        AtomicInteger limit_b = new AtomicInteger(world.getRandom().nextBetween(Config.MIN_METEOR_SPAWN_DISTANCE, Config.MAX_METEOR_SPAWN_DISTANCE));
+
+        world.spawnEntity(getDownwardsMeteorSameDirection(prev.getLeft(), prev.getRight(), world,
+                Math.min(limit_b.get(), limit_a.get()), Math.max(limit_b.get(), limit_a.get()), Config.METEOR_SPAWN_HEIGHT, Config.NATURAL_METEOR_MIN_SIZE, Config.NATURAL_METEOR_MAX_SIZE, Config.HOMING_METEORS));
+        spawned_meteors.getAndIncrement();
+
+        ServerTickEvents.END_SERVER_TICK.register((server) -> {
+            if(ticker.get() == -1 || spawned_meteors.get() >= total){
+                return;
+            }
+            if(ticker.get() == base_spawn_delay+ random_spawn_delay.get()){
+                if(spawned_meteors.get() >= total){
+                    ticker.set(-1);
+                    return;
+                }
+                limit_a.set(world.getRandom().nextBetween(Config.MIN_METEOR_SPAWN_DISTANCE, Config.MAX_METEOR_SPAWN_DISTANCE));
+                limit_b.set(world.getRandom().nextBetween(Config.MIN_METEOR_SPAWN_DISTANCE, Config.MAX_METEOR_SPAWN_DISTANCE));
+
+                world.spawnEntity(getDownwardsMeteorSameDirection(prev.getLeft(), prev.getRight(), world,
+                        Math.min(limit_b.get(), limit_a.get()), Math.max(limit_b.get(), limit_a.get()), Config.METEOR_SPAWN_HEIGHT, Config.NATURAL_METEOR_MIN_SIZE, Config.NATURAL_METEOR_MAX_SIZE, Config.HOMING_METEORS));
+                spawned_meteors.getAndIncrement();
+                ticker.set(0);
+                random_spawn_delay.set(world.getRandom().nextBetween(-10, +10));
+            }
+            ticker.getAndIncrement();
+        });
+
         String message = "message.ohmymeteors.meteor_shower_spawned";
         if(Config.ANNOUNCE_METEOR_SPAWN){
             if(Config.ANNOUNCE_LOCATION){
