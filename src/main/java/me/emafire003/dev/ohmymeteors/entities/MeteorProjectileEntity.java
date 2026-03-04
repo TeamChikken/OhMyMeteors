6f396b00159803cefcb217fc2208fc5e011ea989
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2026-03-04 15:19:42 +0100

    Fixed meteors of size 1 and 2 still spawning a block if onlyReplacAir was true

--- src/main/java/me/emafire003/dev/ohmymeteors/entities/MeteorProjectileEntity.java
+++ src/main/java/me/emafire003/dev/ohmymeteors/entities/MeteorProjectileEntity.java
@@ -14,6 +14,7 @@ import me.emafire003.dev.structureplacerapi.StructurePlacerAPI;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.projectile.*;
 import net.minecraft.world.level.ChunkPos;
@@ -349,8 +350,16 @@ public class MeteorProjectileEntity extends AbstractHurtingProjectile {
         detonateSimple();
         if(!this.level().isClientSide()){
             StructurePlacerAPI placer = getPlacer();
-            //it means the meteor was too small for the structure, a single block has been placed instead
+            //it means the meteor was too small for the structure, placing a single block instead
             if(placer == null){
+                int r = this.getRandom().nextIntBetweenInclusive(1,3);
+                if(r == 1){
+                    this.level().setBlockAndUpdate(BlockPos.containing(this.position()), OMMBlocks.METEORIC_ROCK.defaultBlockState());
+                }else if(r == 2){
+                    this.level().setBlockAndUpdate(BlockPos.containing(this.position()), Blocks.SMOOTH_BASALT.defaultBlockState());
+                }else{
+                    this.level().setBlockAndUpdate(BlockPos.containing(this.position()), Blocks.BLACKSTONE.defaultBlockState());
+                }
                 return;
             }
             placer.loadStructure();
@@ -364,9 +373,19 @@ public class MeteorProjectileEntity extends AbstractHurtingProjectile {
             StructurePlacerAPI placer = getPlacer();
             //it means the meteor was too small for the structure, a single block has been placed instead
             if(placer == null){
+                if(level().getBlockState(this.blockPosition()).isAir()){
+                    int r = this.level().getRandom().nextIntBetweenInclusive(1,3);
+                    if(r == 1){
+                        this.level().setBlockAndUpdate(BlockPos.containing(this.position()), OMMBlocks.METEORIC_ROCK.defaultBlockState());
+                    }else if(r == 2){
+                        this.level().setBlockAndUpdate(BlockPos.containing(this.position()), Blocks.SMOOTH_BASALT.defaultBlockState());
+                    }else{
+                        this.level().setBlockAndUpdate(BlockPos.containing(this.position()), Blocks.BLACKSTONE.defaultBlockState());
+                    }
+                }
                 return;
             }
-            placer.setOnlyReplaceTaggedBlocks(true, OhMyMeteors.AIR_BLOCKS);
+            placer.setOnlyReplaceTaggedBlocks(true, BlockTags.AIR);
             placer.loadStructure();
         }
     }
@@ -375,14 +394,6 @@ public class MeteorProjectileEntity extends AbstractHurtingProjectile {
     public StructurePlacerAPI getPlacer(){
         //If the dimension is even lower than 2, just spawn one block
         if(this.getSize() < 2){
-            int r = this.level().getRandom().nextIntBetweenInclusive(1,3);
-            if(r == 1){
-                this.level().setBlockAndUpdate(BlockPos.containing(this.position()), OMMBlocks.METEORIC_ROCK.defaultBlockState());
-            }else if(r == 2){
-                this.level().setBlockAndUpdate(BlockPos.containing(this.position()), Blocks.SMOOTH_BASALT.defaultBlockState());
-            }else{
-                this.level().setBlockAndUpdate(BlockPos.containing(this.position()), Blocks.BLACKSTONE.defaultBlockState());
-            }
             return null;
         }
 
