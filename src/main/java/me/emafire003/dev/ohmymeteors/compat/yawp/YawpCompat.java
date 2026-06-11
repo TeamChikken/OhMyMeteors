2375db43b89e74606928aaa4b60f95b368a71781
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2026-06-11 19:27:31 +0200

    Renabled flan & yawp

--- src/main/java/me/emafire003/dev/ohmymeteors/compat/yawp/YawpCompat.java
+++ src/main/java/me/emafire003/dev/ohmymeteors/compat/yawp/YawpCompat.java
@@ -1,21 +1,24 @@
 package me.emafire003.dev.ohmymeteors.compat.yawp;
 
+import de.z0rdak.yawp.api.FlagEvaluator;
+import de.z0rdak.yawp.api.events.flag.FlagCheckRequest;
+import de.z0rdak.yawp.api.events.flag.FlagCheckResult;
+import de.z0rdak.yawp.core.flag.FlagState;
+import de.z0rdak.yawp.core.flag.RegionFlag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.BlockPos;
 
 public class YawpCompat {
 
     public static boolean canSpawnHere(ServerLevel world, BlockPos pos){
-        return true;
-        /*TODO FIX when updatee
-        FlagCheckEvent flagCheckEvent = new FlagCheckEvent(pos, RegionFlag.EXPLOSION_ENTITY, world.dimension());
+        FlagCheckRequest flagCheckEvent = new FlagCheckRequest (pos, RegionFlag.EXPLOSION_ENTITY, world.dimension());
         FlagCheckResult flagCheckResult = FlagEvaluator.evaluate(flagCheckEvent);
         FlagState flagState = flagCheckResult.getFlagState();
         if(flagState.equals(FlagState.DENIED)){
             return false;
         }else{
             return true;
-        }*/
+        }
         //return flagState.equals(FlagState.ALLOWED); //returns true if the flagState is allowed, returns false otherwise. which may be the problem
 
     }
